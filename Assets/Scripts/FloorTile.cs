6283c343b4f0f0c4719c5036b11da72b0400e4bc
forrestsmith08 <forrest.smith08@gmail.com>
2018-01-27 14:07:43 -0800

    Added robot movement

--- Assets/Scripts/FloorTile.cs
+++ Assets/Scripts/FloorTile.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FloorTile {
+public class FloorTile : MonoBehaviour {
 
 
 	private int _posY = 0;
 	private int _posX = 0;
 
+
+	void Start()
+	{
+		InitNeighbors();
+	}
+
+	void Update()
+	{
+
+	}
+
 	public FloorTile(int posX, int posY)
 	{
 		_posX = posX;
 		_posY = posY;
-		Debug.Log("Created Tile at " + posX + " " + posY);
 	}
 
 	public virtual void MoveIntoTile(TileMover mover)
@@ -20,4 +30,34 @@ public class FloorTile {
 		
 	}
 
+	public FloorTile GetTile(Vector3 direction)
+	{
+		
+		RaycastHit info;
+
+		if(Physics.Raycast(transform.position + new Vector3(0f,0.2f,0f), direction, out info, 2.0f)){
+			return info.collider.gameObject.GetComponent<FloorTile>();
+		}else{
+			return null;
+		}
+
+	}
+
+	private void InitNeighbors()
+	{
+		RaycastHit info;
+
+		if(Physics.Raycast(transform.position + new Vector3(0f,0.2f,0f), new Vector3(1f,0f,0f), out info, 2.0f)){
+			_eastTile = info.collider.gameObject.GetComponent<FloorTile>();
+		}
+		if(Physics.Raycast(transform.position + new Vector3(0f,0.2f,0f), new Vector3(-1f,0f,0f), out info, 2.0f)){
+			_westTile = info.collider.gameObject.GetComponent<FloorTile>();
+		}
+		if(Physics.Raycast(transform.position + new Vector3(0f,0.2f,0f), new Vector3(0f,0f,1f), out info, 2.0f)){
+			_northTile = info.collider.gameObject.GetComponent<FloorTile>();
+		}
+		if(Physics.Raycast(transform.position + new Vector3(0f,0.2f,0f), new Vector3(0f,0f,-1f), out info, 2.0f)){
+			_southTile = info.collider.gameObject.GetComponent<FloorTile>();
+		}
+	}
 }
--- Assets/Scripts/TileMover.cs
+++ Assets/Scripts/TileMover.cs
@@ -2,18 +2,87 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class TileMover: MonoBehaviour {
 
+	[SerializeField]
+	private FloorTile _currentTile;
 
-	FloorTile _currentTile;
+	[SerializeField]
+	private GameObject _heading;
+
+	private GameObject _target;
+
+	private Quaternion _targetRotation;
+
 
 	// Use this for initialization
 	void Start () {
-		
+		_target = transform.gameObject;
+		_targetRotation = transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(Input.GetKeyDown(KeyCode.W))
+		{
+			MoveForward();
+		}
+
+		if(Input.GetKeyDown(KeyCode.S))
+		{
+			MoveBackward();
+		}
+
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			Rotate(-90f);
+		}
+
+		if(Input.GetKeyDown(KeyCode.D))
+		{
+			Rotate(90f);
+		}
+
+		transform.position = Vector3.Lerp(transform.position, new Vector3(_target.gameObject.transform.position.x, transform.position.y, _target.gameObject.transform.position.z), 7f * Time.deltaTime);
+		transform.rotation = Quaternion.Slerp(transform.rotation, _targetRotation, 10f * Time.deltaTime);
+	}
+
+	private void Move(Vector3 direction)
+	{
+		FloorTile tempTile = _currentTile.GetTile(direction);
+		if(tempTile)
+		{
+			_currentTile = tempTile;
+			_target = tempTile.gameObject;
+		}
+	}
+
+	public void Push(Vector3 direction)
+	{
 		
 	}
+
+	public void MoveForward()
+	{
+		Move(Heading());
+	}
+
+	public void MoveBackward()
+	{
+		Move(Heading() * -1.0f);
+	}
+
+	public void Rotate(float degrees)
+	{
+		_targetRotation *=  Quaternion.AngleAxis(degrees, Vector3.up);
+	}
+
+	private Vector3 Heading()
+	{
+
+		return _heading.gameObject.transform.position - gameObject.transform.position;
+	}
+		
 }
