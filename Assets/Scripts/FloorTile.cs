6562568ee7d33bf5718edf4f129376260569ceb0
forrestsmith08 <forrest.smith08@gmail.com>
2018-01-26 22:58:23 -0800

    Adding Tile and tile manager scripts

---
+++ Assets/Scripts/FloorTile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorTile {
+
+
+	private int _posY = 0;
+	private int _posX = 0;
+
+	public FloorTile(int posX, int posY)
+	{
+		_posX = posX;
+		_posY = posY;
+		Debug.Log("Created Tile at " + posX + " " + posY);
+	}
+
+	public virtual void MoveIntoTile(TileMover mover)
+	{
+		
+	}
+
+}
