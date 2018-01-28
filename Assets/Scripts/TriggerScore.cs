5435bf07451049a2787a5231b42eb5cdde1dd0a7
forrestsmith08 <forrest.smith08@gmail.com>
2018-01-27 22:37:03 -0800

    Added Score Events and hooked up score UI;  Added Ball spawners

--- Assets/Scripts/TriggerScore.cs
+++ Assets/Scripts/TriggerScore.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class TriggerScore : MonoBehaviour
 {
+
+	public PlayerId _playerID;
+	public int _amount;
+
     private void OnTriggerEnter(Collider other)
     {
+		Debug.Log("Collider entered trigger " + other.gameObject.name);
+
+		if(other.tag == "Ball"){
+			SDD.Events.EventManager.Instance.Raise(new ScoreEvent(_playerID, _amount));
+		}
+
+
         //Send Events Here for score
     }
 }
