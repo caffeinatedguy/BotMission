17babfa4602e42289f25aafa781216cb0e13e831
Gregory Chudecke <greg.chudecke@gmail.com>
2018-01-27 21:17:54 -0800

    Adding Score / Ball

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public GameObject[] _activateGameObjects;
+    public GameObject[] _deactivateGameObjects;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        for (int i = 0; i < _activateGameObjects.Length; ++i)
+        {
+            _activateGameObjects[i].gameObject.SetActive(true);
+        }
+
+        for (int i = 0; i < _deactivateGameObjects.Length; ++i)
+        {
+            _deactivateGameObjects[i].gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/TriggerScore.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScore : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //Send Events Here for score
+    }
+}
